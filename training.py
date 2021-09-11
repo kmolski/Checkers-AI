1031bc42c6903971c435486d17c11b5e4f00a2c4
kmolski <starfighter413@gmail.com>
2021-09-11 18:19:53 +0200

    Refactor training code

--- training.py
+++ training.py
@@ -126,7 +126,8 @@ class BaseTrainingSession:
             agent2.use_new_state(node)
 
         logging.info(
-            f"Training session {self.session_index}: game.moves_since_last_capture: {game.moves_since_last_capture}, game.get_winner(): {game.get_winner()}"
+            f"Training session {self.session_index}: game.moves_since_last_capture: "
+            + f"{game.moves_since_last_capture}, game.get_winner(): {game.get_winner()}"
         )
 
         del prev_boards
@@ -147,57 +148,48 @@ class TournamentSession:
         self.c_name = ""
         self.game_index = 0
 
-    def run(self, best, challengers):
-        logging.info(f"Tournament {self.session_index}: starting")
+    def run(self, best):
+        logging.info(f"Tournament {self.session_index}: gathering training data")
+        training_data, _ = self.play_games(best, best)
+        shuffle(training_data)
 
-        challengers_with_scores = []
-        for c in challengers:
-            self.c_name = c.weights_file
-            logging.info(
-                f"Tournament {self.session_index}: playing games against {self.c_name}"
-            )
+        training_data_len = len(training_data)
+        chunk_size = get_chunk_size(training_data_len)
+        chunk_count = training_data_len // chunk_size
 
-            training_data, challenger_win_count = self.play_games(best, c)
-            shuffle(training_data)
+        logging.info(
+            f"Tournament {self.session_index}: training challenger neural net "
+            + f"with {chunk_count} chunks of data, {chunk_size} elements each"
+        )
 
-            training_data_len = len(training_data)
-            chunk_size = get_chunk_size(training_data_len)
-            chunk_count = training_data_len // chunk_size
+        challenger_nn = NeuralNetModel(keras_model=best.model)
+        for (index, chunk) in enumerate(chunks(training_data, chunk_size)):
+            inputs = [datum["input"] for datum in chunk]
+            win_values = [datum["win_value"] for datum in chunk]
+            action_ps = [datum["action_ps"] for datum in chunk]
 
-            logging.info(
-                f"Tournament {self.session_index}, challenger {self.c_name}: training neural net "
-                + f"with {chunk_count} chunks of data, {chunk_size} elements each"
-            )
-            for (index, chunk) in enumerate(chunks(training_data, chunk_size)):
-                inputs = [datum["input"] for datum in chunk]
-                win_values = [datum["win_value"] for datum in chunk]
-                action_ps = [datum["action_ps"] for datum in chunk]
+            logging.info(f"Tournament {self.session_index}: processing chunk {index}")
+            challenger_nn.train(inputs, win_values, action_ps)
 
-                logging.info(
-                    f"Tournament {self.session_index}, challenger {self.c_name}: processing chunk {index}"
-                )
-                best.train(inputs, win_values, action_ps)
-                c.train(inputs, win_values, action_ps)
-
-                del inputs
-                del win_values
-                del action_ps
-                gc.collect()
-
-            challengers_with_scores.append((c, challenger_win_count))
-            del training_data
+            del inputs
+            del win_values
+            del action_ps
             gc.collect()
 
-        return challengers_with_scores
+        del training_data
+        gc.collect()
+
+        logging.info(f"Tournament {self.session_index}: challenging the best net")
+        _, win_count = self.play_games(best, challenger_nn)
+
+        return challenger_nn, win_count
 
     def play_games(self, best, challenger):
         training_data = []
         challenger_win_count = 0
 
         for i in range(self.game_count):
-            logging.info(
-                f"Tournament {self.session_index}, challenger {self.c_name}: playing game {i}"
-            )
+            logging.info(f"Tournament {self.session_index}: playing game {i}")
             self.game_index = i
 
             game = Game()
@@ -213,10 +205,6 @@ class TournamentSession:
             training_data.extend(game_training_data)
             del game_training_data
 
-        logging.info(
-            f"Tournament {self.session_index}, challenger {self.c_name}:"
-            + f" win rate {challenger_win_count / self.game_count}"
-        )
         return training_data, challenger_win_count
 
     def play_until_complete(self, game, best, challenger):
@@ -245,8 +233,38 @@ class TournamentSession:
             challenger_agent.use_new_state(node)
             best_agent.use_new_state(node)
 
+        logging.info(
+            f"Tournament {self.session_index}: game.moves_since_last_capture: "
+            + f"{game.moves_since_last_capture}, game.get_winner(): {game.get_winner()}"
+        )
+
         del prev_boards
         return training_data, game.get_winner()
+
+    def get_best_net(self):
+        logging.info(f"Tournament: picking the best net")
+
+        best, *challengers = [
+            NeuralNetModel(p) for p in Path(".").glob("data/base_training/*")
+        ]
+
+        challengers_with_scores = []
+        for c in challengers:
+            self.c_name = c.weights_file
+            logging.info(f"Tournament: playing games against {self.c_name}")
+
+            _, challenger_win_count = self.play_games(best, c)
+            challengers_with_scores.append((c, challenger_win_count))
+
+        logging.info(f"Tournament: picking best net")
+        challengers_with_scores.sort(key=lambda it: it[1])
+        largest_win_count = challengers_with_scores[-1][1]
+
+        if largest_win_count / self.game_count > 0.5:
+            best = challengers_with_scores.pop()[0]
+
+        logging.info(f"Tournament: got best net: {best.weights_file}")
+        return best
 
     @classmethod
     def train(
@@ -254,28 +272,20 @@ class TournamentSession:
         tournament_count=DEFAULT_TOURNAMENT_COUNT,
         game_count=DEFAULT_GAMES_IN_TOURNAMENT,
     ):
-        best, *challengers = [
-            NeuralNetModel(p) for p in Path(".").glob("data/base_training/*")
-        ]
+        best = TournamentSession(None, game_count).get_best_net()
+
+        best.weights_file = join("data", "tournaments", "best_init_weights")
+        best.persist_weights_to_file()
 
         for i in range(tournament_count):
-            logging.info(f"Tournament {i}: starting")
-            challengers_with_scores = TournamentSession(i, game_count).run(
-                best, challengers
-            )
+            (challenger, wins) = TournamentSession(i, game_count).run(best)
 
-            logging.info(f"Tournament {i}: picking new best net")
-            challengers_with_scores.sort(key=lambda it: it[1])
-            largest_win_count = challengers_with_scores[-1][1]
-            if largest_win_count / game_count > 0.6:
-                new_best = challengers_with_scores.pop()[0]
-                logging.info(
-                    f"Tournament {i}: got new best net: {new_best.weights_file}"
-                )
-                challengers = [c[0] for c in challengers_with_scores] + [best]
-                best = new_best
+            logging.info(f"Tournament {i}: challenger win rate: {wins / game_count}")
+            if wins / game_count > 0.55:
+                logging.info(f"Tournament {i}: got new best net")
+                del best
+                best = challenger
 
-            del challengers_with_scores
             gc.collect()
 
         best.weights_file = join("data", "tournaments", "best_weights")
